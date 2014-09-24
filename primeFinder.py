78bc8b0124fbe06a2bd5caf6460fb91bbb46070e
harper10 <harper10@purdue.edu>
2014-09-24 10:24:32 -0400

    I have the prime function working for now. I didn't add threading capabilities yet, but I did add a function that adds primes to the list. So that is nice. It works well.

--- primeFinder.py
+++ primeFinder.py
@@ -2,6 +2,9 @@
 """
 This is a primeFinder v2.0.
 """
+from math import sqrt
+
+
 
 def p_test_range(num, start, end, plist):
     """
@@ -16,5 +19,43 @@ def p_test_range(num, start, end, plist):
             return False
     return True
 
+def p_test(num, plist):
+    """
+    This function takes in a number and tests whether it is a prime number or not.
+    It also takes in either a list of prime numbers or a file that contains prime
+    numbers. I have not decided yet.
+    It will return True if it is Prime, and False otherwise.
+    I also am planning on using multiple threads to test prime.
+    :param num: int
+    :param plist: [int]
+    :return: boolean
+    """
+    """
+    if plist[-1]**2 < num:
+        print("The list is not populated with enough numbers to test number effectively")
+        return None
+        #TODO I need to make sure that this is the correct thing that I want to return
+    """  #TODO I need to make sure to add this function on the things that call it
+    interval_list = [p for p in plist if p <= sqrt(num)]
+    return p_test_range(num, 0, len(interval_list), interval_list)  #TODO I need to find out how to create threads and replace this call
+
+def populate_plist(plist, end_num):
+    """
+    This function extends the length of plist by testing all of the numbers after the
+    last prime number to the given number
+    :param plist: the current prime list
+    :param end_num: the last number to test
+    :return: [int]
+    """
+    test_nums = [num for num in range(2, end_num+1) if num > plist[-1]]
+    new_primes = [num for num in test_nums if p_test(num, plist)]
+    print(p_test(24, plist))
+    plist += new_primes
+    return plist
+
+
 if __name__ == "__main__":
-    print(p_test_range(10, 3, 3, [2,3,5,7,11,13,17,23]))
+    #print(23**2)
+    #print(sqrt(6203))
+    print(p_test(24, [2, 3, 5, 7, 11, 13, 17, 23]))
+    print(populate_plist([2, 3, 5, 7, 11, 13, 17, 23], 6203))
